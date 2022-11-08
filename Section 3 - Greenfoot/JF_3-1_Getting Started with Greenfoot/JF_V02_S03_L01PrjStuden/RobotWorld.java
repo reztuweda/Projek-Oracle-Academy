988463473ca439003cf71245b8221f37da8efcc3
Made Restu Wedatama <114349183+reztuweda@users.noreply.github.com>
2022-11-08 23:23:15 +0800

    Add files via upload

---
+++ Section 3 - Greenfoot/JF_3-1_Getting Started with Greenfoot/JF_V02_S03_L01PrjStuden/RobotWorld.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (Restu Weda) 
+ * @version (a version number or a date)
+ */
+public class RobotWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public RobotWorld()
+    {    
+        super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Robot robot = new Robot();
+        addObject(robot,300,200);
+    }
+}
